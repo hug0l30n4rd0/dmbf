9696d260108ba55bc0674712221973690bd31874
hugo.dasilva <hugo.dasilva@inss.gov.br>
2019-12-18 16:25:00 -0300

    WIP Modal for inserting/editing spells

--- pages/spell.vue
+++ pages/spell.vue
@@ -209,169 +209,190 @@
                 >
               </template>
               <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
+                <v-form ref="form">
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
 
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          :rules="[rules.required]"
-                          v-model="editedItem.name"
-                          label="Name"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="2">
-                        <v-select
-                          label="Level"
-                          :items="[
-                            { id: 0, name: 'Cantrip' },
-                            { id: 1, name: '1st' },
-                            { id: 2, name: '2nd' },
-                            { id: 3, name: '3rd' },
-                            { id: 4, name: '4th' },
-                            { id: 5, name: '5th' },
-                            { id: 6, name: '6th' },
-                            { id: 7, name: '7th' },
-                            { id: 8, name: '8th' },
-                            { id: 9, name: '9th' }
-                          ]"
-                          :rules="[rules.required]"
-                          v-model="editedItem.level"
-                          item-value="id"
-                          item-text="name"
-                        ></v-select>
-                      </v-col>
-                      <v-col ols="12" sm="6" md="4">
-                        <v-select
-                          label="School"
-                          :items="filter.schools"
-                          :rules="[rules.required]"
-                          v-model="editedItem.school"
-                          item-value="id"
-                          item-text="name"
-                        >
-                        </v-select>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="2">
-                        <v-switch
-                          v-model="editedItem.isConcentration"
-                          label="Concentration"
-                        />
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
-                        <v-text-field
-                          v-model="editedItem.castingValue"
-                          label="Casting Time"
-                        ></v-text-field>
-                        <v-select
-                          :items="filter.castingTimes"
-                          :rules="[rules.required]"
-                          v-model="editedItem.castingTimes"
-                          item-value="id"
-                          item-text="name"
-                          class="ml-2"
-                        >
-                        </v-select>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
-                        <v-select
-                          label="Range"
-                          :items="filter.ranges"
-                          :rules="[rules.required]"
-                          v-model="editedItem.range"
-                          item-value="id"
-                          item-text="name"
-                          class="mr-2"
-                        >
-                        </v-select>
-                        <v-text-field
-                          label="Distance (ft.)"
-                          v-model="editedItem.rangeDistance"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
-                        <v-select
-                          label="Duration"
-                          :items="filter.durations"
-                          :rules="[rules.required]"
-                          v-model="editedItem.duration"
-                          item-value="id"
-                          item-text="name"
-                          class="mr-2"
-                        >
-                        </v-select>
-                        <v-text-field
-                          v-model="editedItem.durationValue"
-                        ></v-text-field>
-                        <v-select
-                          :items="filter.durationTypes"
-                          :rules="[rules.required]"
-                          v-model="editedItem.durationType"
-                          item-value="id"
-                          item-text="name"
-                          class="ml-2"
-                        >
-                        </v-select>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="3">
-                        <p>Components</p>
-                        <v-btn-toggle dense multiple>
-                          <v-btn
-                            @click="editedItem.isVisual = !editedItem.isVisual"
-                            class="groupSearchType"
-                            >V</v-btn
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            :rules="[rules.required]"
+                            v-model="editedItem.name"
+                            label="Name"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="2">
+                          <v-select
+                            label="Level"
+                            :items="[
+                              { id: 0, name: 'Cantrip' },
+                              { id: 1, name: '1st' },
+                              { id: 2, name: '2nd' },
+                              { id: 3, name: '3rd' },
+                              { id: 4, name: '4th' },
+                              { id: 5, name: '5th' },
+                              { id: 6, name: '6th' },
+                              { id: 7, name: '7th' },
+                              { id: 8, name: '8th' },
+                              { id: 9, name: '9th' }
+                            ]"
+                            :rules="[rules.required]"
+                            v-model="editedItem.level"
+                            item-value="id"
+                            item-text="name"
+                          ></v-select>
+                        </v-col>
+                        <v-col ols="12" sm="6" md="4">
+                          <v-select
+                            label="School"
+                            :items="filter.schools"
+                            :rules="[rules.required]"
+                            v-model="editedItem.school"
+                            item-value="id"
+                            item-text="name"
                           >
-                          <v-btn
-                            @click="
-                              editedItem.isSomatic = !editedItem.isSomatic
+                          </v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="2">
+                          <v-switch
+                            v-model="editedItem.isConcentration"
+                            label="Concentration"
+                          />
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
+                          <v-text-field
+                            v-model="editedItem.castingValue"
+                            label="Casting Time"
+                          ></v-text-field>
+                          <v-select
+                            :items="filter.castingTimes"
+                            :rules="[rules.required]"
+                            v-model="editedItem.castingTime"
+                            item-value="id"
+                            item-text="name"
+                            class="ml-2"
+                          >
+                          </v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
+                          <v-select
+                            label="Range"
+                            :items="filter.ranges"
+                            :rules="[rules.required]"
+                            v-model="editedItem.range"
+                            item-value="id"
+                            item-text="name"
+                            class="mr-2"
+                          >
+                          </v-select>
+                          <v-text-field
+                            label="Distance (ft.)"
+                            v-model="editedItem.rangeDistance"
+                            :disabled="
+                              editedItem.range == null || editedItem.range != 3
                             "
-                            class="groupSearchType"
-                            >S</v-btn
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" class="d-flex flex-row">
+                          <v-select
+                            label="Duration"
+                            :items="filter.durations"
+                            :rules="[rules.required]"
+                            v-model="editedItem.duration"
+                            item-value="id"
+                            item-text="name"
+                            class="mr-2"
                           >
-                          <v-btn
-                            @click="
-                              editedItem.isMaterial = !editedItem.isMaterial
+                          </v-select>
+                          <v-text-field
+                            v-model="editedItem.durationValue"
+                            :disabled="
+                              editedItem.duration == null ||
+                                !(
+                                  editedItem.duration == 3 ||
+                                  editedItem.duration == 4
+                                )
                             "
-                            class="groupSearchType"
-                            >M</v-btn
+                          ></v-text-field>
+                          <v-select
+                            :items="filter.durationTypes"
+                            :rules="[rules.required ? true : false]"
+                            :disabled="
+                              editedItem.duration == null ||
+                                !(
+                                  editedItem.duration == 3 ||
+                                  editedItem.duration == 4
+                                )
+                            "
+                            v-model="editedItem.durationType"
+                            item-value="id"
+                            item-text="name"
+                            class="ml-2"
                           >
-                        </v-btn-toggle>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="editedItem.components"
-                          :disabled="!editedItem.isMaterial"
-                          label="Material Component Description"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="2">
-                        <v-switch
-                          v-model="editedItem.isRitual"
-                          label="Ritual"
-                        />
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="12">
-                        <v-textarea
-                          ref="description"
-                          v-model="editedItem.description"
-                          :rules="[rules.required]"
-                          outlined
-                          label="Description"
-                          value=""
-                        ></v-textarea>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
+                          </v-select>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="3">
+                          <p>Components</p>
+                          <v-btn-toggle dense multiple>
+                            <v-btn
+                              @click="
+                                editedItem.isVisual = !editedItem.isVisual
+                              "
+                              class="groupSearchType"
+                              >V</v-btn
+                            >
+                            <v-btn
+                              @click="
+                                editedItem.isSomatic = !editedItem.isSomatic
+                              "
+                              class="groupSearchType"
+                              >S</v-btn
+                            >
+                            <v-btn
+                              @click="
+                                editedItem.isMaterial = !editedItem.isMaterial
+                              "
+                              class="groupSearchType"
+                              >M</v-btn
+                            >
+                          </v-btn-toggle>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="editedItem.components"
+                            :disabled="!editedItem.isMaterial"
+                            label="Material Component Description"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="2">
+                          <v-switch
+                            v-model="editedItem.isRitual"
+                            label="Ritual"
+                          />
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="12">
+                          <v-textarea
+                            ref="description"
+                            v-model="editedItem.description"
+                            :rules="[rules.required]"
+                            outlined
+                            label="Description"
+                            value=""
+                          ></v-textarea>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                </v-form>
 
                 <v-card-actions>
                   <v-spacer></v-spacer>
@@ -630,16 +651,16 @@ export default {
     },
     editedItem: {
       name: '',
-      level: null,
-      school: null,
+      level: 0,
+      school: 1,
       isConcentration: false,
       isRitual: false,
-      castingValu: '',
-      castingTime: null,
-      range: null,
-      rangeDistance: '',
-      rangeMetric: null,
-      duration: null,
+      castingValue: null,
+      castingTime: 1,
+      range: 1,
+      rangeDistance: null,
+      rangeMetric: 1,
+      duration: 1,
       durationValue: null,
       durationType: null,
       isVerbal: false,
@@ -734,7 +755,7 @@ export default {
       })
     },
     getSpellRanges() {
-      this.$http.get('/resourverbalce/spell-range/').then((res) => {
+      this.$http.get('/resource/spell-range/').then((res) => {
         this.filter.ranges = res.data
       })
     },
@@ -754,14 +775,14 @@ export default {
       })
     },
 
-    editItem(item) {
-      this.editedIndex = this.items.indexOf(item)
-      this.editedItem = Object.assign({}, item)
+    edit(entity) {
+      this.editedIndex = this.items.indexOf(entity)
+      this.editedItem = Object.assign({}, entity)
       this.dialog = true
     },
 
-    deleteItem(item) {
-      const index = this.items.indexOf(item)
+    delete(entity) {
+      const index = this.items.indexOf(entity)
       confirm('Are you sure you want to delete this item?') &&
         this.items.splice(index, 1)
     },
@@ -777,16 +798,17 @@ export default {
     save() {
       if (this.$refs.form.validate()) {
         if (this.editedIndex > -1) {
+          // console.log('~~~~ Edited Item:')
+          // console.log(JSON.stringify(this.editedItem))
           Object.assign(this.items[this.editedIndex], this.editedItem)
         } else {
+          console.log('~~~~ Inserted Item:')
           console.log(JSON.stringify(this.editedItem))
-
           this.$http
-            .post('/item/', JSON.stringify(this.editedItem), {
+            .post('/spell/', JSON.stringify(this.editedItem), {
               headers: { 'Content-Type': 'application/json; charset=utf-8' }
             })
             .then((res) => {
-              console.log('salvo com sucesso')
               this.getData()
             })
             .catch((err) => console.log(err))
